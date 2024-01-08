335e162ab95b463b012e1a21d344d615a8504553
ad-dilax <antonydilaxsan@gmail.com>
2024-01-08 17:48:35 +0530

    leave module complete

--- Staff/staff_leave_list.php
+++ Staff/staff_leave_list.php
@@ -1,32 +1,64 @@
+<?php
+include 'db_connection.php';
+session_start();
+//if(!isset($_SESSION["id"])){
+    //header("Location:login.php");
+    //exit();
+    //}
+if(!isset($_SESSION['id'])){
+    //$staff_id = $_SESSION['id'];
+	$staff_id = 1;
+
+    $sql = "SELECT * FROM `staffs` WHERE staff_id = '$staff_id'"; 
+
+    $result = mysqli_query($conn, $sql);
+    if($row=mysqli_fetch_assoc($result)){
+
+    
+    $last_name = $row['last_name'];
+    $contact_no = $row['contact_no'];
+    $email = $row['email'];
+    }
+    
+    else{
+      header("Location: staff_dashboard.php");
+      
+    }
+
+
+}
+
+?>
+
+
 <!doctype html>
 <html lang="en">
    <head>
       <!-- Required meta tags -->
       <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <!-- favicon -->
-        <link rel="icon" type="image/png" href="../assets/images/favicon.png">
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="assets/css/bootstrap.min.css" media="all">
-        <!-- Fonts Awesome CSS -->
-        <link rel="stylesheet" type="text/css" href="assets/css/all.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <!-- google fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,400&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,400&display=swap" rel="stylesheet">
-        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-        <!-- Custom CSS -->
-        <link rel="stylesheet" type="text/css" href="style.css">
-        <title>Woocurs Academy LK</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <!-- favicon -->
+      <link rel="icon" type="image/png" href="../assets/images/favicon.png">
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="assets/css/bootstrap.min.css" media="all">
+      <!-- Fonts Awesome CSS -->
+      <link rel="stylesheet" type="text/css" href="assets/css/all.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <!-- google fonts -->
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,400&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,400&display=swap" rel="stylesheet">
+      <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+      <!-- Custom CSS -->
+      <link rel="stylesheet" type="text/css" href="style.css">
+      <title>Woocurs Academy</title>
 </head>
 <body>
-
     <!-- start Container Wrapper -->
     <div id="container-wrapper">
         <!-- Dashboard -->
         <div id="dashboard" class="dashboard-container">
             <div class="dashboard-header sticky-header">
                 <div class="content-left  logo-section pull-left">
-                    <h1><a href="../index.php"><img src="assets/images/logo.png" alt=""></a></h1>
+                    <h1><a href="../index.html"><img src="assets/images/logo.png" alt=""></a></h1>
                 </div>
                 <div class="heaer-content-right pull-right">
                     <div class="search-field">
@@ -37,7 +69,9 @@
                             </div>
                         </form>
                     </div>
-                   
+                    <div class="dropdown">
+                        
+                    </div>
                     <div class="dropdown">
                         <a class="dropdown-toggle" data-toggle="dropdown">
                             <div class="dropdown-item profile-sec">
@@ -48,9 +82,9 @@
                         </a>
                         <div class="dropdown-menu account-menu">
                             <ul>
-                                <li><a href="admin_edit.php"><i class="fas fa-cog"></i>Edit Profile</a></li>
+                                <li><a href="staff_edit.php"><i class="fas fa-cog"></i>Edit Profile</a></li>
                                 <li><a href="profile_card.php"><i class="fas fa-user-tie"></i>Profile</a></li>
-                                <li><a href="admin_change_password.php"><i class="fas fa-key"></i>Password</a></li>
+                                <li><a href="staff_change_password.php"><i class="fas fa-key"></i>Password</a></li>
                                 <li><a href="login.php"><i class="fas fa-sign-out-alt"></i>Logout</a></li>
                             </ul>
                         </div>
@@ -62,30 +96,27 @@
                 <div id="dashboard-Navigation" class="slick-nav"></div>
                 <div id="navigation" class="navigation-container">
                     <ul>
-                        <li><a href="admin_dashboard.php"><i class="fa fa-chart-bar"></i>Dashboard</a> </li>
-                        <li><a href="admin_stafflist.php"><i class="fas fa-user"></i>Staff</a> </li>
-                        <li><a href="admin_studentlist.php"><i class="fa fa-users"></i>Students</a></li> 
-                        <li><a href="courses.php"><i class="fa fa-book"></i> Courses </a></li>   
-                        <li><a href="admin_leave_list.php"><i class="fa fa-calendar-times-o"></i>Manage Staff Leave</a></li>
-                        <li><a href="admin_paymentlist.php"> <i class='fa fa-credit-card'></i> Payments </a></li>
-                        <li><a href="admin_salary.php"><i class="fas fa-money"></i> Salary </a></li>
+                        <li><a href="staff_dashboard.php"><i class="far fa-chart-bar"></i> Dashboard</a></li>
+                        <li><a href="staff_edit.php"><i class="fas fa-user"></i>Edit Profile</a> </li>
+                        <li><a href="staff_course.php"> <i class='fa fa-book'></i>Course</a></li>
+                        <li ><a href="staff_notes.php"> <i class='fa fa-sticky-note-o'></i>Notes</a></li>
+                        <li><a href="staff_assigment.php"><i class="fa fa-tasks"></i>Assigment</a></li>
+                        <li><a href="staff_calander.php"><i class="fa fa-calendar" aria-hidden="true"></i> Calander </a></li>
+                        <li><a href="staff_salary.php"><i class="fa fa-money"></i>Salary</a></li>
+                        <li class="active-menu"><a href="staff_leave.php"><i class="fa fa-calendar-times-o"></i>Leaves</a></li>
                         <li><a href="login.php"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
                     </ul>
                 </div>
             </div>
-            <div class="db-info-wrap">
+			<div class="db-info-wrap">
                 <div class="row">
-                    <!-- Item -->
-                  
-                    <!-- Item -->
                     <div class="col-xl-3 col-sm-6">
                         <div class="db-info-list">
                             <div class="dashboard-stat-icon bg-green">
-                                <i class='bx bxs-check-square'></i>
-                            
+                                <i class='fa fa-calendar-times-o'></i>
                             </div>
                             <div class="dashboard-stat-content">
-                                <h4><a href="admin_activated_leave_list.php">Aproved Leave</a></h4>
+                                <h4><a href="staff_leave.php">Apply Leave</a></h4>
                                 
                             </div>
                         </div>
@@ -94,52 +125,31 @@
                    
                     <div class="col-xl-3 col-sm-6">
                         <div class="db-info-list">
-                            <div class="dashboard-stat-icon bg-red">
-                                <i class='bx bxs-comment-x'></i>
-                                
+                            <div class="dashboard-stat-icon bg-blue">
+                                <i class='fa fa-eye'></i>
                             </div>
                             <div class="dashboard-stat-content">
-                                <h4><a href="admin_rejected_leave_list.php">Rejected Leave</a></h4>
-                               
+                                <h4><a href="staff_leave_list.php">View Leave</a></h4>
                             </div>
                         </div>
                     </div>
                 </div>
-                   
-                                        
-                                
-                   
-            
+            </div>
+        <div class="db-info-wrap">
+			<div>
                 <div class="row">
                     <div class="col-lg-12">
-                        <div class="dashboard-box">
-                            <h4>Staff Leave Details  </h4>
-                            
-                            <div class="table-responsive">
-                                <table class="table">
+                        <div class="dashboard-box user-form-wrap">
+                            <h4>Leave Details</h4>
+							<table class="table">
                                     <thead>
                                         <tr>
-                                            <th>Staff ID</th>
-											<th>Staff Name</th>
                                             <th>Leave Type</th>
                                             <th>Start Date</th>
 											<th>End Date</th>
                                             <th>Status</th>
                                         </tr>
                                     </thead>
-									<?php
-                                    include "db_connection.php";
-                                    $sql = mysqli_query($conn,"SELECT * FROM staffs WHERE status = 'Active'");
-
-                                    ?>
-
-                                    <tbody>
-                                        <?php
-
-                                    while($row = $sql->fetch_assoc()) {
-										$last_name = $row["last_name"];
-                                        ?>
-										
                                     <?php
                                     include "db_connection.php";
                                     $result = mysqli_query($conn,"SELECT * FROM leaves");
@@ -156,8 +166,6 @@
 
                                         <?php 
                                         // output data of each row
-                                            echo'<td>'.$row["staff_id"].'</td>';
-                                            echo'<td>'.$last_name.'</td>';
 											echo'<td>'.$row["type"].'</td>';
 											echo'<td>'.$row["start_date"].'</td>';
 											echo'<td>'.$row["end_date"].'</td>';
@@ -173,44 +181,41 @@
                                             {
                                                 echo'<td><span class="badge badge-primary">'.$row["status"].'</span></td>';
                                             }
-                                            ?>
 
-                                            <td>
-                                                <a href="admin_leave_view.php ?leave_id=<?php  echo $row["leave_id"]; ?> "><span class="badge badge-success"><i class="far fa-eye"></i></span></a>
-                                                
-                                            </td>
+                                            ?>
+    
                                         </tr>
                                         <?php
                                     }
-									}
                                     $conn->close();
                                         ?>
                                         
                                     </tbody>
                                 </table>
-                            </div>
                         </div>
                     </div>  
                 </div>
-                
-                
+            </div>
+			
             <!-- Content / End -->
+
             <!-- Copyrights -->
             <div class="copyrights">
-               Copyright © 2023 Woocurs Academy LK. All rights reserveds.
-            </div>
+                Copyright © 2023 Woocurs Academy. All rights reserveds.
+             </div>
         </div>
         <!-- Dashboard / End -->
     </div>
     <!-- end Container Wrapper -->
-    <!-- *Scripts* -->
+    <!-- end Container Wrapper -->
     <script src="assets/js/jquery-3.2.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
     <script src="assets/js/canvasjs.min.js"></script>
-    <script src="assets/js/chart.js"></script>
     <script src="assets/js/counterup.min.js"></script>
     <script src="assets/js/jquery.slicknav.js"></script>
     <script src="assets/js/dashboard-custom.js"></script>
 </body>
+
+<!-- Mirrored from cyclonethemes.com/demo/html/padhai/dashboard-addtour.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 02 Feb 2020 09:01:50 GMT -->
 </html>

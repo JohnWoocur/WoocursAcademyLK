5d28fb673f56241f34602b85e33a9faa955ed284
Nusra Faizar <nusrafathi10@gmail.com>
2024-01-18 16:10:49 +0530

    notes and assignment

--- Staff/staff_add_assignment.php
+++ Staff/staff_add_assignment.php
@@ -1,5 +1,9 @@
 <?php include("../protect.php");
 notAuthenticated("staff", "login.php"); // if user not authenticated and redirect to login
+
+
+
+
 ?>
 
 <!doctype html>
@@ -91,17 +95,27 @@ notAuthenticated("staff", "login.php"); // if user not authenticated and redirec
                                 <div class="row">
                                     <div class="col-sm-6">
                                         <div class="form-group">
-                                            <label>Course ID</label>
-                                            <input name="cid" class="form-control" type="text" required>
+                                         
+                                            <label for="course_id"> Course ID </label>
+                                            <select id="course_id" name="course_id" required>
+                                            
+                                             <?php 
+                                             require "db_connection.php";
+                                             
+                                                $query = "SELECT course_id FROM courses";
+                                                $result = mysqli_query($conn, $query);
+                                                while ($row = $result->fetch_assoc()) {
+                                                    echo "<option value='" . $row['course_id'] . "'>" . $row['course_id'] . "</option>";
+                                                }
+                                                
+                                                ?>
+                                            
+
+
+                                            </select>
                                         </div>
                                     </div>
-                                    <!-- <div class="col-sm-6">
-                                        <div class="form-group">
-                                            <label>Lecture ID</label>
-                                            <input name="lid" class="form-control" type="text" required>
-                                        </div>
-                                    </div>
-                                     -->
+                                    
                                     <div class="col-sm-6">
                                         <div class="form-group">
                                             <label>Post Date</label>

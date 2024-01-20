09132daeee7d4c527d8f2012378c599cd85b94b5
linthujan <53619838+linthujan@users.noreply.github.com>
2024-01-20 14:32:44 +0530

    payment added status

--- db/woocursacademylk.sql
+++ db/woocursacademylk.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 19, 2024 at 06:51 PM
+-- Generation Time: Jan 20, 2024 at 10:01 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -150,7 +150,8 @@ CREATE TABLE `payments` (
   `slip_no` varchar(255) NOT NULL,
   `date` date NOT NULL,
   `gender` enum('male','female','other') NOT NULL,
-  `photo` varchar(255) NOT NULL
+  `photo` varchar(255) NOT NULL,
+  `status` varchar(50) NOT NULL DEFAULT 'pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
